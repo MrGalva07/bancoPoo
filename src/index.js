0d98e5552a54ac89b7ccad5a98c5703814a20eb8
MrGalva07 <lucas.galvao0707@gmail.com>
2024-09-13 13:59:26 -0300

    ignore correciton

--- src/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var ContaBancaria_1 = require("./models/ContaBancaria");
-var c1 = new ContaBancaria_1.ContaBancaria();
-c1.numero = "12345-6";
-console.log(c1);
-c1.agencia = "5234-6";
-console.log(c1);
